e6febf6a7caddcda5f85fbd7d7b410074c7431f1
abigailvo2005 <abigailvo2005@gmail.com>
2025-12-11 22:34:58 +0700

    fix edit course subject with status only planned

--- src/components/Admin/Management/index.js
+++ src/components/Admin/Management/index.js
@@ -1,5 +1,4 @@
 // Management components
 export { default as AssessmentManagement } from './AssessmentManagement';
-export { default as CourseManagement } from './CourseManagement';
 export { default as ReportsManagement } from './ReportsManagement';
 export { default as SQAAuditManagement } from './SQAAuditManagement';
